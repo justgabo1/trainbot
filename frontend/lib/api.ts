c5e15286e6ac6ebaf1b4a4cd51a7429414380644
batagbo <bata.gabo@gmail.com>
2026-06-11 22:31:46 +0200

    Update API URL to new Render URL

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://trainbot2026.onrender.com/api'
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://trainbot-4rob.onrender.com/api'
 })
 
 api.interceptors.request.use(config => {
